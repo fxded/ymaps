2f40e111ac37cc436e93675a2ea950376ed229d7
Vasiliy Votintsev <fxded@mail.ru>
2022-04-07 15:09:36 +0300

    develop simple map

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { YMaps, Map, Placemark } from 'react-yandex-maps'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <YMaps query={{ns: 'use-load-option',
+                       // load: 'geoObject.addon.balloon, \
+                       //       control.ZoomControl,\
+                       //       control.FullscreenControl, \
+                        load:      'package.full'}}>
+          <Map defaultState={{ 
+            center: [55, 37], 
+            zoom: 9,
+            controls: ['zoomControl', 'fullscreenControl'],
+          }}
+          // modules={['control.FullscreenControl']}
+          >
+            <Placemark
+              defaultGeometry={[55, 37]}
+              properties={{balloonContentBody: "this is just text"}}
+            />
+          </Map>
+        </YMaps>
       </header>
     </div>
   );
